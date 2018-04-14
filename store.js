37e868ffd97599efe7eb68059a7dfb729862699c
Dylan <nielsen.dylan@gmail.com>
2018-04-13 18:56:32 -0700

    Add queue to store simDB

--- store.js
+++ store.js
@@ -3,25 +3,69 @@
 const catData = require('./scratch-data/catdata');
 const dogData = require('./scratch-data/dogdata');
 
+class _QueueNode {
+  constructor(value, next, prev) {
+    this.value = value;
+    this.next = next;
+    this.prev = prev;
+  }
+}
+
+class Queue {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
+
+  enqueue(value) {
+    let node = new _QueueNode(value);
+    if (this.head === null) {
+      this.head = node;
+    }
+    if (this.tail) {
+      node.next = this.tail;
+      this.tail.prev = node;
+    }
+    this.tail = node;
+  }
+
+  dequeue() {
+    if (this.head === null) return null;
+    let returnNode = this.head;
+    this.head = returnNode.prev;
+    if (returnNode === this.tail) {
+      this.tail = null;
+    }
+    return returnNode.value;
+  }
+}
+
+let catQueue = new Queue();
+catData.forEach(item => {
+  catQueue.enqueue(item);
+});
+
+let dogQueue = new Queue();
+dogData.forEach(item => {
+  dogQueue.enqueue(item);
+});
+
 const store = {
-  catData,
-  dogData,
+  catData: catQueue,
+  dogData: dogQueue,
   getDog: function() {
-    return dogData[0];
+    return this.dogQueue.head.value;
   },
   getCat: function() {
-    return catData[0];
+    return this.catQueue.head.value;
   },
   deleteDog: function() {
-    let temp = this.dogData[0];
-    this.dogData.push(temp);
-    this.dogData.splice(0,1);
-    
+    let temp = this.dogQueue.dequeue();
+    this.dogQueue.enqueue(temp);
   },
   deleteCat: function() {
-    let temp = this.catData[0];
-    this.catData.push(temp);
-    this.catData.splice(0,1);
+    let temp = this.catQueue.dequeue();
+    this.catQueue.enqueue(temp);
   }
 };
 
